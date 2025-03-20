897b09d2df81921333bb4734298abe199a63409c
ArunMAT86 <arunmathew009@gmail.com>
2025-03-20 15:34:14 -0400

    Add componets and routes

--- poke-api/src/app/app.routes.ts
+++ poke-api/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TeamComponent } from './team/team.component';
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'team' ,component: TeamComponent},
+    
+    {path: 'details/:id', component: PokemonDetailsComponent}
+];
